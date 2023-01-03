4157646d4262310ffaee7135b39b87c98518157f
AlexCherniuk <60302722+AlexCherniuk@users.noreply.github.com>
2023-01-03 18:53:07 +0100

    added navigation in slider

--- dist/js/script.js
+++ dist/js/script.js
@@ -2417,7 +2417,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // Timer 
 
-  const deadline = '2022-12-28';
+  const deadline = '2023-01-06:22:00';
   function getTimeRemaining(endtime) {
     const t = Date.parse(endtime) - Date.parse(new Date());
     let days, hours, minutes, seconds;
@@ -2790,8 +2790,24 @@ window.addEventListener('DOMContentLoaded', () => {
   const prev = document.querySelector('.offer__slider-prev');
   const totalSlides = document.querySelector('#total');
   const currentSlides = document.querySelector('#current');
+  const slider = document.querySelector('.offer__slider');
   let slideIndex = 1;
   let offset = 0;
+  const dots = [];
+  slider.style.position = 'relative';
+  const indicators = document.createElement('ol');
+  slider.append(indicators);
+  indicators.classList.add('carousel-indicators');
+  for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement('li');
+    dot.classList.add('dot');
+    dot.setAttribute('data-slide-to', i + 1);
+    if (i == 0) {
+      dot.style.opacity = 1;
+    }
+    indicators.append(dot);
+    dots.push(dot);
+  }
   if (slides.length < 10) {
     totalSlides.textContent = `0${slides.length}`;
     currentSlides.textContent = `0${slideIndex}`;
@@ -2807,7 +2823,7 @@ window.addEventListener('DOMContentLoaded', () => {
   slidesField.style.display = 'flex';
   slidesField.style.transition = '0.5s all';
   slidesWrapper.style.overflow = 'hidden';
-  const widthToNum = +width.slice(0, width.length - 2); // 
+  const widthToNum = +width.replace(/\D/g, ''); // 
 
   next.addEventListener('click', () => {
     if (offset == widthToNum * (slides.length - 1)) {
@@ -2827,6 +2843,10 @@ window.addEventListener('DOMContentLoaded', () => {
     } else {
       currentSlides.textContent = slideIndex;
     }
+
+    //navigation dots 
+    dots.forEach(dot => dot.style.opacity = '.5');
+    dots[slideIndex - 1].style.opacity = 1;
   });
   prev.addEventListener('click', () => {
     if (offset == 0) {
@@ -2845,7 +2865,25 @@ window.addEventListener('DOMContentLoaded', () => {
     } else {
       currentSlides.textContent = slideIndex;
     }
+    dots.forEach(dot => dot.style.opacity = '.5');
+    dots[slideIndex - 1].style.opacity = 1;
   });
+  dots.forEach(dot => {
+    dot.addEventListener('click', e => {
+      const slideTo = e.target.getAttribute('data-slide-to');
+      slideIndex = slideTo;
+      offset = widthToNum * (slideTo - 1);
+      slidesField.style.transform = `translateX(-${offset}px)`;
+      if (slideIndex < 10) {
+        currentSlides.textContent = `0${slideIndex}`;
+      } else {
+        currentSlides.textContent = slideIndex;
+      }
+      dots.forEach(dot => dot.style.opacity = '.5');
+      dots[slideIndex - 1].style.opacity = 1;
+    });
+  });
+  localStorage.setItem('key', 'value');
 
   ///////version 2 
   // showSlides(slideIndex);
